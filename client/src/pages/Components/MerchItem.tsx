af30012c10a7a321508f6bfe9996218c714968bb
James Hocking <jhoc6907@uni.sydney.edu.au>
2023-06-22 14:30:12 +1000

    startingmerch

---
+++ client/src/pages/Components/MerchItem.tsx
@@ -0,0 +1,15 @@
+
+
+function MerchItem(){
+    return(
+        <div className="merch-item-box">
+            <img />
+            <h1>Name</h1>
+            <>Size</>
+            <>Price</>
+            <>Add to cart</>
+        </div>
+    )
+}
+
+export default MerchItem;
--- client/src/pages/Merch.tsx
+++ client/src/pages/Merch.tsx
@@ -1,12 +1,17 @@
 import React, { useState, useEffect } from "react";
 import NavBar from "./Components/NavBar";
 import Header from "./Components/Header";
+import MerchItem from "./Components/MerchItem";
 
 function Merch(){
     return (
         <>
             <NavBar />
             <Header title="Merch" description="Look good, play good. Choose from our range of top quality products below."/>
+            <div className="merch-box">
+                <MerchItem />
+                <MerchItem />
+            </div>
         </>
     )
 }
