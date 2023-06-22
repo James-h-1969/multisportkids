435e77066e4757c390a01942802283e8f89b0be7
James Hocking <jhoc6907@uni.sydney.edu.au>
2023-06-22 15:16:59 +1000

    moremerch

--- client/src/pages/Components/MerchItem.tsx
+++ client/src/pages/Components/MerchItem.tsx
@@ -1,13 +1,46 @@
+import React, { useState } from "react";
+import { Button, DropdownButton, ButtonGroup, Dropdown } from "react-bootstrap";
+
+interface MerchItemProps{
+    name: string,
+    price: string,
+    image: React.ReactElement;
+}
 
 
-function MerchItem(){
+const MerchItem: React.FC<MerchItemProps> = ({name, price, image}) => {
+    const [selectedOption, setSelectedOption] = useState("Choose size");
+
+    function handleOptionSelect(eventkey: string | null){
+        if (eventkey){
+            setSelectedOption(eventkey);
+        }
+    }
     return(
         <div className="merch-item-box">
-            <img />
-            <h1>Name</h1>
-            <>Size</>
-            <>Price</>
-            <>Add to cart</>
+            <div className="merch-img">{image}</div>
+            <div className="merch-text">
+                <h1>{name}</h1>
+                <div className="merch-price">{price}</div>
+                <div className="merch-size">
+                    <a><span className="merch-size-title">Size</span></a>
+                    <DropdownButton
+                        as={ButtonGroup}
+                        title={selectedOption}
+                        onSelect={handleOptionSelect}
+                        variant="secondary"
+                        >
+                        <Dropdown.Item eventKey="Small">Small</Dropdown.Item>
+                        <Dropdown.Item eventKey="Medium">Medium</Dropdown.Item>
+                        <Dropdown.Item eventKey="Large">Large</Dropdown.Item>
+                        <Dropdown.Item eventKey="Extra Large">Extra Large</Dropdown.Item>
+                    </DropdownButton>
+                </div>
+                <div className="merch-quantity">
+
+                </div>
+                <Button style={{backgroundColor: "#46768E"}}>Add to cart</Button>
+            </div>
         </div>
     )
 }
--- client/src/pages/Merch.tsx
+++ client/src/pages/Merch.tsx
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from "react";
 import NavBar from "./Components/NavBar";
 import Header from "./Components/Header";
 import MerchItem from "./Components/MerchItem";
+import "./Merch.css";
+import hoodie from "./assets/AFLkids hoodie.jpg";
+import shirt from "./assets/AFLkids playing shirt.jpg";
 
 function Merch(){
     return (
@@ -9,8 +12,8 @@ function Merch(){
             <NavBar />
             <Header title="Merch" description="Look good, play good. Choose from our range of top quality products below."/>
             <div className="merch-box">
-                <MerchItem />
-                <MerchItem />
+                <MerchItem name="Hoodie" price="$35" image={<img src={hoodie}/>}/>
+                <MerchItem name="Training Shirt" price="$25" image={<img src={shirt}/>}/>
             </div>
         </>
     )
