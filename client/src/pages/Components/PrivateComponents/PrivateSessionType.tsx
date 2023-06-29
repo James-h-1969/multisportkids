24afa6d8a8740c551e04dd279353a8b1d37d2243
James Hocking <jhoc6907@uni.sydney.edu.au>
2023-06-29 22:05:43 +1000

    cleaned up fonts and worked on part 3 of private

--- client/src/pages/Components/PrivateComponents/PrivateSessionType.tsx
+++ client/src/pages/Components/PrivateComponents/PrivateSessionType.tsx
@@ -1,12 +1,64 @@
-import SessionType from "./SessionType";
+import { useState } from "react";
+import { InputGroup, FormControl } from "react-bootstrap";
 
 function PrivateSessionType(){
+    const [isActive, setisActive] = useState([true, false, false, false, false]);
+    const [activeIndex, setactiveIndex] = useState(0);
+    const [amountofKids, setamountofKids] = useState(1);
+
+    const handleClick = (index:number) => {
+        let newState = isActive;
+        if (!isActive[4]){
+            setamountofKids(1);
+        }
+        if (index != activeIndex){
+            newState[index] = true;
+            newState[activeIndex] = false;
+            setactiveIndex(index);
+            setisActive(newState);
+            return;
+        }
+        newState[activeIndex] = false;
+        setactiveIndex(-1);
+        setisActive(newState);
+    }
     return(
         <div className="private-session-type-box">
             <h1><span className='step1'>Step 3: Pick Session Type</span></h1>
-            <SessionType />
+            <div className={isActive[0] ? "session-type-active": "session-type-deactive"} onClick={() => handleClick(0)}>
+                <h1><span className="private-session-type">1 on 1</span></h1>
+                <h2><span className="private-session-price">$80</span></h2>
+            </div>
+            <div className={isActive[1] ? "session-type-active": "session-type-deactive"} onClick={() => handleClick(1)}>
+                <h1><span className="private-session-type">2 on 1</span></h1>
+                <h2><span className="private-session-price">$110</span></h2>
+            </div>
+            <div className={isActive[2] ? "session-type-active": "session-type-deactive"} onClick={() => handleClick(2)}>
+                <h1><span className="private-session-type">3 on 1</span></h1>
+                <h2><span className="private-session-price">$140</span></h2>
+            </div>
+            <div className={isActive[3] ? "session-type-active": "session-type-deactive"} onClick={() => handleClick(3)}>
+                <h1><span className="private-session-type">5 on 1</span></h1>
+                <h2><span className="private-session-price">$200</span></h2>
+            </div>
+            <div className={isActive[4] ? "session-type-active": "session-type-deactive"} onClick={() => handleClick(4)}>
+                <h1><span className="private-session-type">... on 1</span></h1>
+                <h2><span className="private-session-price">$80</span></h2>
+                <a>+ $30 per extra AFL kid</a>
+                <div className="kid-quantity">
+                    <InputGroup>
+                        <FormControl
+                            type="number"
+                            value={amountofKids}
+                            onChange={(e) => setamountofKids(parseInt(e.target.value))}
+                            style={{}}
+                        />
+                    </InputGroup>
+                </div>
+            </div>
         </div>
-    )
+
+    );
 }
 
 export default PrivateSessionType;
--- client/src/pages/Private.tsx
+++ client/src/pages/Private.tsx
@@ -5,6 +5,8 @@ import PrivateLocation from "./Components/PrivateComponents/PrivateLocation";
 import PrivateTimetable from "./Components/PrivateComponents/PrivateTimetable";
 import PrivateSessionType from "./Components/PrivateComponents/PrivateSessionType";
 import Footer from "./Components/Footer";
+import {Button} from "react-bootstrap"; 
+import "./Components/Components.css";
 
 function Private(){
     const desc = "Get one of our best coaches for personalised assistance. Tailored help to get the most out of your AFL kid."
@@ -15,6 +17,9 @@ function Private(){
             <PrivateLocation />
             <PrivateTimetable />
             <PrivateSessionType />
+            <div className="private-add">
+                    <Button size="lg" style={{backgroundColor: "#46768E"}}>Add to cart</Button>
+            </div>
             <Footer />
         </>
     )
