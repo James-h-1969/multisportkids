72171dfb7aff1f507bbb8e495fd11e44ad110b76
James-h-1969 <102572039+James-h-1969@users.noreply.github.com>
2023-11-27 11:29:13 +1100

    Update Header.tsx

--- client/src/pages/Components/Header.tsx
+++ client/src/pages/Components/Header.tsx
@@ -18,8 +18,8 @@ const Header: React.FC<HeaderProps> = ({title, description}) => {
           <div className="" style={{ paddingTop:mediaQueries.mobile?"100px":"120px", paddingLeft:mediaQueries.mobile?"30px":"90px", zIndex:"-100"}}>
             <h1><span className="heading-text" style={{fontSize:mediaQueries.mobile?"30px":"90px", color:ColorScheme.defaultColor}}>{title}</span></h1>
           </div>
-          <div className="" style={{width:mediaQueries.mobile?"90%":"90%", paddingLeft:mediaQueries.mobile?"30px":"90px", fontWeight:"bold"}}>
-            <a><span className="heading-description" style={{fontSize:mediaQueries.mobile?"10px":"15px"}}>{description}</span></a>
+          <div className="" style={{width:mediaQueries.mobile?"90%":"90%", paddingLeft:mediaQueries.mobile?"30px":"90px", fontWeight:"bold", lineHeight:mediaQueries.mobile?"0.7":"1"}}>
+            <a><span className="heading-description" style={{fontSize:mediaQueries.mobile?"8px":"15px"}}>{description}</span></a>
           </div>
     </>
   )
